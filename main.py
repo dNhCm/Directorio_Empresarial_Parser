8839b14c2433cf2b227a3b3d1ec59cbbd776e473
dhmmmhb <dhmaguran@gmail.com>
2023-09-15 11:21:34 +0300

    final commit

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 
-# Imports
-import logging, os
+import logging
+import os
+from logging import Logger
+
 import requests
 from lxml import html
 from fake_useragent import UserAgent
 from configparser import ConfigParser
-import csv, json
+import csv
+import json
 
 
 # Global vars
 config = ConfigParser()
 config.read('config.ini')
+logger: Logger
+
 
 # Global funcs
 def parse(session: requests.Session, url: str, headers: dict, xpathes: dict[str: str]) -> dict[str: list[str]]:
+    """
+    Just parsing url with session and headers by xpathes
+
+    :param session:
+    :param url:
+    :param headers:
+    :param xpathes: dict of calling of xpath and value of it
+    :return: responses by calling of xpathes. For example, xpathes = {'activity': 'some_xpath'}, then return {'activity: 'response'}
+    """
+
     answer = {section: [] for section in xpathes}
 
     api = session.get(url=url, headers=headers)
@@ -26,25 +41,56 @@ def parse(session: requests.Session, url: str, headers: dict, xpathes: dict[str:
 
 
 def transform(response: dict[str: list[str]]) -> dict[str: str]:
+    """
+    Format response
+
+    :param response:
+    :return: if in section by xpath no elements, then it will return empty str, but if there is elements, then return only first one.
+    """
+
     for section in response:
         if not len(response[section]) == 0:
             response[section] = response[section][0]
-        else: response[section] = ''
+        else:
+            response[section] = ''
 
     return response
+
+
+def check_for_req(response: dict[str: list[str]]) -> bool:
+    """
+    Checks if all required items are present
+
+    :param response:
+    :return: if the response does not contain at least one required item, it returns false, otherwise true.
+    """
+
+    req = config['PRIORITY']['req'].split(' ')
+    is_there = True
+
+    for section in response:
+        if section in req:
+            if len(response[section]) == 0:
+                is_there = False
+
+    return is_there
+
 
 # Tech Classes
 
 class Dispatcher:
+    """
+    Manager of all work
+    """
+
     @staticmethod
     def start():
         System.logging()
-        System.preparation_csvfile()
-        System.preparation_datajson()
+        System.preparing_csvfile()
+        System.preparing_json()
 
     @staticmethod
     def working_on():
-        Activity.init_site_session()
         UI.main()
 
     def main(self):
@@ -53,6 +99,10 @@ class Dispatcher:
 
 
 class System:
+    """
+    Working with files and logging
+    """
+
     @staticmethod
     def logging():
         global logger
@@ -64,7 +114,8 @@ class System:
         )
 
     @staticmethod
-    def preparation_csvfile():
+    def preparing_csvfile():
+        # If file doesn't exist, then create a new one by template
         if not os.path.isfile('data/data.csv'):
             with open('data/data.csv', 'w', encoding='windows-1252', newline='') as csvfile:
                 csvfile = csv.writer(csvfile)
@@ -73,7 +124,8 @@ class System:
             logger.info('data.csv was created successfully')
 
     @staticmethod
-    def preparation_datajson():
+    def preparing_json():
+        # If file doesn't exist, then create a new one by template
         if not os.path.isfile('data/data.json'):
             data = {'last_panel_page': {}, 'companies_href': {}}
             with open('data/data.json', 'w') as datajson:
@@ -82,10 +134,15 @@ class System:
     @staticmethod
     def delete_cache():
         os.remove(path='data/data.json')
-        System.preparation_datajson()
+        System.preparing_json()
 
 
 class UI:
+    """
+    UI for choice task
+    """
+    task: int  # Chosen task by user
+
     @staticmethod
     def parse_companies_href():
         Activity.parse_companies_href()
@@ -99,35 +156,40 @@ class UI:
         System.delete_cache()
 
     @classmethod
-    def web(self):
-        self.task = int(input(
-'''\nКакое задание сейчас выберете:
+    def choice(cls):
+        cls.task = int(input('''\nКакое задание сейчас выберете:
 Спарсить href - 1
 Спарсить info - 2
 Очистить кэш - 3
 Выйти - 4
 
-Ваш Ответ: '''
-        ))
+Ваш Ответ: '''))
 
     @classmethod
-    def main(self):
+    def main(cls):
         while True:
-            self.web()
-            if self.task == 1: self.parse_companies_href()
-            elif self.task == 2: self.parse_companies_info()
-            elif self.task == 3: self.delete_cache()
-            else: break
+            cls.choice()
+            if cls.task == 1:
+                cls.parse_companies_href()
+            elif cls.task == 2:
+                cls.parse_companies_info()
+            elif cls.task == 3:
+                cls.delete_cache()
+            else:
+                break
 
 
 class Activity:
-    @classmethod
-    def init_site_session(self):
-        self.site_session = config['PARSE']['site_session']
+    """
+    Parsing hrefs and info of chosen activity from site
+    """
 
-    @classmethod
-    def parse_companies_href(self):
-        logger.info('preparating to parsing companies href')
+    site_session = config['PARSE']['site_session']
+
+    @staticmethod
+    def parse_companies_href():
+        logger.info('preparing to parsing companies href')
+        # Get data for parsing
         url = config['PARSE']['url']
         headers = {
             'User-Agent': UserAgent().random
@@ -136,11 +198,15 @@ class Activity:
 
         with requests.Session() as session:
             logger.info('session was created')
-            cookie = {'domain': 'panel-empresarial.institutofomentomurcia.es', 'name': 'JSESSIONID', 'path': '/IFM-panel-directorio', 'value': self.site_session}
+            # Work with cookies for connecting parser to your session
+            cookie = {'domain': 'panel-empresarial.institutofomentomurcia.es', 'name': 'JSESSIONID', 'path': '/IFM-panel-directorio', 'value': Activity.site_session}
             session.cookies.set(**cookie)
             logger.info('logged in was successful')
 
+            # Get current activity
             activity = parse(session=session, url=f'{url}1', headers=headers, xpathes={'activity': config['PARSE']['activity_xpath']})['activity'][0]
+
+            # Get previous last page where parser was stopped in parsing
             try:
                 with open('data/data.json', 'r') as datajson:
                     last_page = json.load(datajson)['last_panel_page'][activity]
@@ -148,79 +214,89 @@ class Activity:
                 last_page = 1
                 logger.error(f'may be no last page.. Error: {ex}')
 
+            # Start to parse for companies href by pages
             page = last_page
             errors = 0
-            logger.info('preparating was finished successfully')
+            logger.info('preparing was finished successfully')
             logger.info('start parsing...')
-            while errors < 2:
+            while errors < 2:  # If three errors were thrown in a row then stop parsing
                 try:
                     response = parse(session=session, url=f'{url}{page}', headers=headers, xpathes=xpathes)
-                    if len(response['companies_href']) == 0: raise Exception
+
+                    # Checking if it parses no company then raise error, because page is empty
+                    if len(response['companies_href']) == 0:
+                        raise Exception
+
+                    # Add our list of hrefs to companies_hrefs in data.json
                     with open('data/data.json', 'r') as datajson:
                         data = json.load(datajson)
-                    try: data['companies_href'][activity] += response['companies_href']
-                    except: data['companies_href'][activity] = response['companies_href']
+
+                    try:
+                        data['companies_href'][activity] += response['companies_href']
+                    except NameError:
+                        data['companies_href'][activity] = response['companies_href']
                     data['last_panel_page'][activity] = page
+
                     with open('data/data.json', 'w') as datajson:
                         json.dump(data, datajson)
+
+                    # reset errors count, because page was parsed successfully; and log
                     errors = 0
-                    logger.info(f'{page}th page was parsed succesfully')
+                    logger.info(f'{page}th page was parsed successfully')
                 except Exception as ex:
                     errors += 1
                     logger.error(f'error parsing in {page}th page. Error: {ex}')
-                finally: page += 1
-            else: logger.info('companies href was parsed')
+                finally:  # Go to next page to parse
+                    page += 1
+            else:
+                logger.info('companies href was parsed')
 
-    @classmethod
-    def parse_companies_info(self):
-        logger.info('preparating to parsing companies info')
+    @staticmethod
+    def parse_companies_info():
+        logger.info('preparing to parsing companies info')
+        # Get data for parsing
         url = config['PARSE']['main_url']
         headers = {
             'User-Agent': UserAgent().random
         }
         xpathes = {section: config['COMPANY'][section] for section in config['COMPANY']}
-        with open('data/data.json', 'r') as datajson:
-            data = json.load(datajson)
+        data = json.load(open('data/data.json', 'r'))
         sections = ['activity', 'link'] + [section for section in config['COMPANY']]
-        logger.info('preparating was finished successfully')
+        logger.info('preparing was finished successfully')
         
-        logger.info('start to parsing href`s')
-        for activity in data['companies_href']:
+        logger.info('start to parsing href\'s')
+        # Parsing companies info
+        for activity in data['companies_href']:  # Parse for every activity
             for href in data['companies_href'][activity]:
                 with requests.Session() as session:
+                    # Parsing info from href
                     response = parse(session=session, url=f'{url}{href}', headers=headers, xpathes=xpathes)
-                    req = self.check_for_req(response=response)
+
+                    # Checkpoint for the presence of required items
+                    req = check_for_req(response=response)
+
+                    # Format response
                     response = transform(response=response)
                     response['activity'] = activity
                     response['link'] = f'{url}{href}'
-                    if not req:
+
+                    # Upload our data if there are all required items; if no, then skip
+                    if req:
                         with open('data/data.csv', 'a', encoding='windows-1252', newline='') as csvfile:
                             csvfile = csv.DictWriter(csvfile, fieldnames=sections)
                             csvfile.writerow(response)
                     else:
-                        logger.info(f'doesnt intresting in {href}')
+                        logger.info(f'doesnt interesting in {href}')
                         continue
                 logger.info(f'parsed {href} href')
             logger.info(f'parsed all of href')
 
-    @staticmethod
-    def check_for_req(response: dict[str: list[str]]) -> bool:
-        req = config['PRIORITY']['req'].split(' ')
-        bool = True
 
-        for section in response:
-            if section in req:
-                if len(response[section]) == 0:
-                    bool = False
-
-        return bool
-
-
-# Main
 def main():
     dp = Dispatcher()
     dp.main()
     logger.info('\033[34m\033[43m\033[1m💙💛 Слава Україні! 💙💛\033[0m')
+
 
 if __name__ == '__main__':
     main()
